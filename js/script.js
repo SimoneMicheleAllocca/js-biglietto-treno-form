2cd66dc2918adc56ab0279d87afeccf8dc269ea0
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-03-26 19:55:10 +0100

    add: js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const sendBtn = document.getElementById("send");
+    console.log(sendBtn);
+
+sendBtn.addEventListener("click", function(){
+
+
+const kmInputElem = document.getElementById("km");
+    console.log(kmInputElem);
+
+const kmValue = kmInputElem.value;
+    console.log(kmValue);
+    
+})
