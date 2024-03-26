ef39a6bf4d93df95e53f665f1f356036d64ff698
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-03-26 20:11:41 +0100

    add: btn+finalmessage

--- js/script.js
+++ js/script.js
@@ -9,5 +9,32 @@ const kmInputElem = document.getElementById("km");
 
 const kmValue = kmInputElem.value;
     console.log(kmValue);
-    
-})
+
+    const ageInputElem = document.getElementById("user-age");
+    console.log(ageInputElem);
+
+const ageValue = ageInputElem.value;
+    console.log(kmValue);
+
+    const price = kmValue * 0.21; 
+    console.log(price);
+
+let discount = " "; 
+
+if(ageValue < 18){
+    discount = price * 0.20;
+} else if (ageValue > 65) {
+    discount = price * 0.40 ;
+} else {
+    discount = " ";
+}
+
+let priceDiscount = price - discount;
+console.log(priceDiscount);
+
+const finalPrice= priceDiscount.toFixed(2);
+    resultMessage = "Il tuo biglietto costerà: " + finalPrice;
+
+    document.getElementById("testo-biglietto").innerHTML = resultMessage;
+
+});
